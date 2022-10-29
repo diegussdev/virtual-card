a8aaf4b7e6d42bcaef467738d94b28294086828c
diegussdev <diegussdev@gmail.com>
2022-10-28 22:21:42 -0300

    Add ProfileTest

---
+++ app/tests/Feature/ProfileTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProfileTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_store_profile()
+    {
+        $response = $this->from('/generate')->post('/profile-store', [
+            'name' => 'John',
+            'linkedin_url' => 'https://linkedin.com/',
+            'github_url' => 'https://github.com/',
+            'about' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+        ]);
+
+        $response->assertRedirectContains('qr-code');
+    }
+}
